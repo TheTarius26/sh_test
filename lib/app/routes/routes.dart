5aab38cc53802f0d34d328a87c9bad436c17feff
Rhaka Novyansyah Dewanto <rhakadewanto26@gmail.com>
2022-05-12 00:45:16 +0700

    add home

--- lib/app/routes/routes.dart
+++ lib/app/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sh_test/app/ui/entry/view/entry_page.dart';
+import 'package:sh_test/app/ui/home/view/home_page.dart';
 import 'package:sh_test/app/ui/login/view/login_page.dart';
 import 'package:sh_test/app/ui/register/view/register_page.dart';
 
@@ -7,6 +8,7 @@ final Map<String, Widget Function(BuildContext)> routes = {
   '/': (context) => const EntryPage(),
   '/login': (context) => const LoginPage(),
   '/register': (context) => const RegisterPage(),
+  '/home': (context) => const HomePage(),
 };
 
-const initialRoute = '/';
+const initialRoute = '/home';
---
+++ lib/app/ui/home/view/home_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:sh_test/app/common/color.dart';
+import 'package:sh_test/app/common/constant.dart';
+import 'package:sh_test/app/common/text_style.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          padding: const EdgeInsets.all(20),
+          decoration: const BoxDecoration(
+            gradient: linearAppGradient,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Hi, Rhaka',
+                    style: textStyleTitle.copyWith(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 24),
+                  ),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.logout,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              const SizedBox(height: kPadding * 0.25),
+              Text(
+                'Current Date: ${DateTime.now().day}-${DateTime.now().month}-${DateTime.now().year}',
+                style: textStyleBody.copyWith(
+                  fontSize: 14,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: kPadding),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: kPadding * 0.5),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(kRadiusCircle),
+                ),
+                child: TableCalendar(
+                  focusedDay: DateTime.now(),
+                  firstDay: DateTime.utc(2010, 10, 16),
+                  lastDay: DateTime.utc(2030, 3, 14),
+                  headerStyle: const HeaderStyle(
+                    formatButtonVisible: false,
+                    titleCentered: true,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
